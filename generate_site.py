8cc95db683772476c3233792b3d4fdf337d18204
Joe <1817@duck.com>
2026-08-29 14:28:13 -0400

    moved home buttons into drawer

--- generate_site.py
+++ generate_site.py
@@ -102,14 +102,14 @@ def generate_html_site(input_filename, output_dir="docs"):
 	<div class="progress-bar" id="progressBar"></div>
 
 	<div class="drawer" id="sideDrawer" onclick="event.stopPropagation()">
-		<button class="drawer-btn" onclick="renderHome()" title="Go Home">
+		<button class="drawer-btn hidden" id="drawerHome" onclick="renderHome()" title="Go Home">
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 				<path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
 				<polyline points="9 22 9 12 15 12 15 22"/>
 			</svg>
 		</button>
-		<button class="drawer-btn disabled" id="drawerPrev" title="Prev Chapter">&#9664;</button>
-		<button class="drawer-btn disabled" id="drawerNext" title="Next Chapter">&#9654;</button>
+		<button class="drawer-btn hidden" id="drawerPrev" title="Prev Chapter">&#9664;</button>
+		<button class="drawer-btn hidden" id="drawerNext" title="Next Chapter">&#9654;</button>
 		<button class="drawer-btn" onclick="toggleFullScreenWithFade()" title="Full Screen">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 				<path d="M8 3H5a2 2 0 0 0-2 2v3" />
@@ -118,6 +118,17 @@ def generate_html_site(input_filename, output_dir="docs"):
 				<path d="M16 21h3a2 2 0 0 0 2-2v-3" />
 			</svg>
 		</button>
+		<button class="drawer-btn danger" id="drawerReset" onclick="resetProgress()" title="Reset Progress">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+				<path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/>
+				<path d="M3 3v5h5"/>
+			</svg>
+		</button>
+		<a href="https://github.com/joe1817/test2" class="drawer-btn" id="drawerGithub" title="Github Repo">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+				<path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"/>
+			</svg>
+		</a>
 	</div>
 
 	<div class="main-wrapper">
@@ -134,12 +145,6 @@ def generate_html_site(input_filename, output_dir="docs"):
 		index_html += f'                <a onclick="loadChapter({ch["num"]})" class="chapter-box" data-chapter="{ch["num"]}">Ch. {ch["num"]}</a>\n'
 
 	index_html += """            </div>
-
-			<div class="reset-container">
-				<button id="reset-progress-btn" onclick="resetProgress()">Reset Reading Progress</button>
-			</div>
-
-			<p class="github-link"><a href="https://github.com/joe1817/test2">Github</a></p>
 		</div>
 
 		<div id="chapter-container" class="chapter-view"></div>
